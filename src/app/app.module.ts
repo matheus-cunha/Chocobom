ccded7a227ccb402d17119e963985b31d59212ea
matheus-cunha <matheus.cunha.sjc@gmail.com>
2019-04-08 20:23:31 -0300

    Responsive-att

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { BoxComponent } from './box/box.component';
+import { FooterComponent } from './footer/footer.component';
+import { PromoBoxComponent } from './promo-box/promo-box.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    BoxComponent,
+    FooterComponent,
+    PromoBoxComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/promo-box/promo-box.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-promo-box',
+  templateUrl: './promo-box.component.html',
+  styleUrls: ['./promo-box.component.css']
+})
+export class PromoBoxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
